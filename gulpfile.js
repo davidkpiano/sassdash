5ea9a6da06913f8f21610de4fa97ac2d80a7631f
David Khourshid <davidkpiano@gmail.com>
2015-01-14 17:04:36 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+gulp.task('scss', function() {
+    gulp.src('scss/*.scss')
+        .pipe(sass({
+            style: 'expanded',
+            "sourcemap=none": true
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['scss'], function() {
+    gulp.watch('scss/*.scss', ['scss']);
+});

af195b1fa0843abf5bb6bc3fa09eba778ce58cd2
David Khourshid <davidkpiano@gmail.com>
2015-02-18 11:00:24 -0500

    Added _to-string unit tests, fixed tests gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 
-gulp.task('scss', function() {
-    gulp.src('**/*.scss')
-        .pipe(sass({
-            style: 'expanded',
-            "sourcemap=none": true,
-            trace: true
-        }))
-        .pipe(gulp.dest('css'));
-});
-
 gulp.task('default', ['scss'], function() {
     gulp.watch('scss/*.scss', ['scss']);
 });
 
-gulp.task('test', ['scss'], function() {
-    gulp.src('**/*.scss')
-        .pipe(sass())
+gulp.task('test', function() {
+    sass('tests/', {
+            style: 'expanded',
+            "sourcemap=none": true,
+            trace: true
+        })
         .pipe(gulp.dest('css'));
 
     gulp.watch('**/*.scss', ['test']);
